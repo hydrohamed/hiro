156cfbe28ae08cd4aabe4724eec304af31e1197b
Hamed Samsami <samsami.hd@gmail.com>
2022-12-03 10:12:25 +0330

    Bug fixes and some improvements

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -12,9 +12,8 @@ var queue string
 var rootCmd = &cobra.Command{
 	Use:     "hiro",
 	Version: "0.0.1",
-	Short:   "A powerful, fast, and lightweight download manager and accelerator.",
-	Long: `A powerful, fast, and lightweight download manager and accelerator built with
-			love by samsamihd in Go.
+	Short:   "A powerful, fast, and lightweight download manager and accelerator written in Go!",
+	Long: `A powerful, fast, and lightweight download manager and accelerator written in Go!
 			Complete documentation is available at https://github.com/samsamihd/hiro`,
 	Run: func(cmd *cobra.Command, args []string) {
 
@@ -22,7 +21,7 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
-	rootCmd.PersistentFlags().StringVar(&queue, "queue", "q", "Add download task to specific queue")
+	rootCmd.PersistentFlags().StringVar(&queue, "queue", "", "Add a download task to a specific queue")
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 		os.Exit(1)
